dfbf33b9936dc46505fe1e9136149bf26ce2cf7f
zycorn <41660921+zycorn@users.noreply.github.com>
2022-08-27 17:45:51 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -181,7 +181,7 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         if birth_day == 0:
             birthday_data = "今天{}生日哦，祝{}生日快乐！".format(value["name"], value["name"])
         else:
-            birthday_data = "距离{}的生日还有{}天".format(value["name"], birth_day)
+            birthday_data = "距离{}的生日还有{}天\n".format(value["name"], birth_day)
         # 将生日数据插入data
         data["data"][key] = {"value": birthday_data, "color": get_color()}
     headers = {
